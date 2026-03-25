133a07204420bee84c0a74a7bef3930aa3d70561
Syed Safeer-Ul-Islam <2024S-bcet-039@ssuet.edu.pk>
2026-03-25 05:29:24 +0500

    Moved supabase keys to environment variables

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL  = 'https://tcwtnhjanhnscmbvvcnl.supabase.co';
-const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjd3RuaGphbmhuc2NtYnZ2Y25sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5OTgzNTcsImV4cCI6MjA4OTU3NDM1N30.ISoZswx5SyFNvlj0mKjehEkujNueJe1HUPTWLUkgrLU';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
